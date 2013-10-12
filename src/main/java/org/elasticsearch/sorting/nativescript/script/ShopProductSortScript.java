15088b4a070450b60e6b52a46d5b1d6ad6141f28
kaka <huxinghai1988@gmail.com>
2013-10-12 11:32:47 +0800

    fixed sort algo

---
+++ src/main/java/org/elasticsearch/sorting/nativescript/script/ShopProductSortScript.java
@@ -0,0 +1,28 @@
+package main.java.org.elasticsearch.sorting.nativescript.script;
+
+import java.util.Map;
+
+import main.java.org.elasticsearch.sorting.nativescript.script.ActivitySortScript.SortScript;
+
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.script.AbstractDoubleSearchScript;
+import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.NativeScriptFactory;
+
+public class ShopProductSortScript implements NativeScriptFactory {
+	@Override
+	public ExecutableScript newScript(@Nullable Map<String, Object> params) {	
+		return new SortScript();
+	}
+
+	private static class SortScript extends AbstractDoubleSearchScript {
+		
+		public SortScript(){
+			
+		}
+		
+		public double runAsDouble(){
+			return 0.001;
+		}
+	}
+}
